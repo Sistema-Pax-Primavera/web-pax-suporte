e2194aa73df48defe789e11c7a5b62841d4d6ef6
Diogo Perez <perezdiogo1@gmail.com>
2024-04-05 14:25:31 -0400

    primeiro commit

---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const RoutesApp = () => (
+    <BrowserRouter basename="pax-primavera/suporte">
+        <Routes>
+            {/* <Route exact path="*" element={ } /> */}
+        </Routes>
+    </BrowserRouter>
+);
+
+export default RoutesApp;
